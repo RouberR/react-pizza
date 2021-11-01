ec0940cdddcf0b0a249828cb6214662448cae214
RouberR <Rouber1998@gmail.com>
2021-11-01 03:33:50 +0300

    PizzaBlock, axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from "react";
 import { Route } from "react-router";
+import axios from "axios";
 import {Header} from "./components";
 import {Cart, Home} from "./Page";
 
 
 
 function App() {
+  const [pizza, setPizza] = React.useState()
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({data}) => {
+      setPizza(data.pizzas)
+    })
+  }, [])
+  console.log(pizza)
   return (
     <div className="wrapper">
       <Header/>
     <div className="content">
-    <Route exact path="/" > <Home/> </Route>
+    <Route exact path="/" render={() => <Home pizza={pizza}/> } />  
     <Route exact path="/cart"> <Cart/> </Route>
      
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Categories} from "./Menu/Categories.jsx";
 export {default as Sorting} from "./Menu/Sorting.jsx";
 export {default as Header} from "./Header/Header.jsx";
+export {default as PizzaBlock} from "./PizzaBlock/PizzaBlock.jsx";
